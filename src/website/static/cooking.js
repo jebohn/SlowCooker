d4046b7ac9e12d41d54ba5935a579af87a172c93
Adam Vekris <vekrisadam@gmail.com>
2026-02-16 17:11:18 -0500

    Improved javascript

--- src/website/static/cooking.js
+++ src/website/static/cooking.js
@@ -1,1 +1,13 @@
 // blah blah blah
+const tempElement = document.getElementById("temp");
+const timeElement = document.getElementById("time");
+const pythonUpdate = new EventSource('/log/stream');
+
+
+pythonUpdate.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+
+    tempElement.textContent = event.curr_temp;
+    timeElement.textContent = event.timestamp;
+}
